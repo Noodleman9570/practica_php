b74d5f8e9499c5bc2cc4f916b55158be0e964501
Kevin Saavedra <kevinsaavedra55@gmail.com>
2022-08-25 23:42:20 -0400

    Configuracion de archivo de logs del servidor

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+    error_reporting(E_ALL);
+
+    ini_set('ignore_repeated_errors', TRUE);
+
+    ini_set('display_errors', FALSE);
+
+    ini_set('log_errors', TRUE);
+
+    ini_set("error_log", __DIR__ . "/php-error.log");
+
+    error_log("Inicio de aplicacion");
+
+    require_once 'libs/app.php';
+
+?>
+
+
