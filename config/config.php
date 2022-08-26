269d4c0e1ea307381569694dfdb7e351a557c25c
Kevin Saavedra <kevinsaavedra55@gmail.com>
2022-08-26 08:26:50 -0400

    Se agrego un archivo de configuracines de entorno config.php

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+// Definicion de constante globales de todo el proyecto
+define('URL', 'http://localhost');
+
+define('HOST', 'localhost');
+define('DB', 'movies_database');
+define('USER', 'root');
+define('PASSWORD', 'root');
+define('CHARSET', 'utf8mb4');
