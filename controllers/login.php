0314db1c68e433413d29a867cd6803a67b795cf2
Kevin Saavedra <kevinsaavedra55@gmail.com>
2022-08-26 08:25:57 -0400

    Se agrego las clases padres para los modelos y las vistas que heredaran de ellas

---
+++ controllers/login.php
@@ -0,0 +1,18 @@
+<?php
+
+class Login extends Controller{
+
+    function __construct()
+    {
+        parent::__construct();
+        error_log('Login::construct -> inicio de Login');
+    }
+
+    function render()
+    {
+        error_log('Login::render -> Carga el index de login');
+        $this->view->render('login/index');
+    }
+
+}
+
